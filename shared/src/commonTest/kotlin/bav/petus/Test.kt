9ddcb42064e4e6051292fb89bd6b4fbc76a6674c
Aleksandr Bakanov <bakanov.aleksandr@gmail.com>
2024-06-05 23:17:44 +0200

    Add empty KMM project

---
+++ shared/src/commonTest/kotlin/bav/petus/Test.kt
@@ -0,0 +1,12 @@
+package bav.petus
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
