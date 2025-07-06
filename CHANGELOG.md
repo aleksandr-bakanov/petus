0b944f7a8d8bcfbe2e65240d5b13d1d237c3ae25
Aleksandr Bakanov <bakanov.aleksandr@gmail.com>
2025-07-06 22:36:18 +0200

    Release 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.0] - 2025-07-06
+
+### Added
+
+- New pet: bober
+- Quest status screen
+
+### Fixed
+
+- Weather fetch error
+
 ## [1.0.2] - 2025-06-10
 
 ### Fixed
--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "bav.petus.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
     }
     buildFeatures {
         compose = true
