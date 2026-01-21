f6555154c0b2f9a52450db89d3a9918d21e324c1
Aleksandr Bakanov <bakanov.aleksandr@gmail.com>
2026-01-21 21:16:04 +0100

    Release 1.3.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.3.3] - 2026-01-21
+
+### Fixed
+
+- Fix for Dragon Age quest
+
 ## [1.3.2] - 2025-12-26
 
 ### Fixed
--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "bav.petus.android"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.3.2"
+        versionCode = 9
+        versionName = "1.3.3"
     }
     buildFeatures {
         compose = true
